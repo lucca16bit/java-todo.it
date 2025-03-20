e60c9b8c321ffea92a30f014c70697e73ef66ec3
lucas <lucaston2323@gmail.com>
2025-03-20 17:17:52 -0300

    chore: renomear path controller

--- src/main/java/br/com/api/todolist/controller/AuthController.java
+++ src/main/java/br/com/api/todolist/controller/AuthController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
-public class UserController {
+@RequestMapping("/auth")
+public class AuthController {
 
     @Autowired
     private AuthenticationManager manager;
