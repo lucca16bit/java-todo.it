3174d98c18a8f39e5ce140aa6a1dbb1a791e612e
lucas <lucaston2323@gmail.com>
2025-03-24 17:09:09 -0300

    fix: permissões do spring security e mudanças no CORS

--- src/main/java/br/com/api/todolist/config/CorsConfiguration.java
+++ src/main/java/br/com/api/todolist/config/CorsConfiguration.java
@@ -10,9 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS");
     }
 }
