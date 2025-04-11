de007077062bade48aecc9e9e0cb5559526f20af
Lucas Gonçalves <103429796+lucca16bit@users.noreply.github.com>
2025-04-10 21:04:07 -0300

    chore: update Cors Configuration

--- src/main/java/br/com/api/todolist/security/SecurityConfigurations.java
+++ src/main/java/br/com/api/todolist/security/SecurityConfigurations.java
@@ -44,7 +44,7 @@ public class SecurityConfigurations {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://java-todolist-production.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
