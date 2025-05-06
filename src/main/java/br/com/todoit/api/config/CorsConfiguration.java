4fecb7f65d851d0432785941aeb5e6e8d7b2ecd6
lucas <lucaston2323@gmail.com>
2025-05-06 17:35:42 -0300

    refactor: renomear pacote main do projeto

--- src/main/java/br/com/todoit/api/config/CorsConfiguration.java
+++ src/main/java/br/com/todoit/api/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200/")
+                .allowedOrigins("https://todoit-simply.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
